de582ad99fdf9c9b07387062f262f60dd26acd0e
Derrick1451 <haylederrick14@gmail.com>
2023-04-11 16:32:41 +0300

    Update js file

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const hamburger = document.querySelector('.loose');
 const popUp = document.querySelector('.pop-up');
 const closeIcon = document.querySelector('.close-icon');
-const navLinks = document.querySelector('.nav-links');
-
+const navLinks = document.querySelectorAll('.nav-links');
 
 hamburger.addEventListener('click', () => {
-    popUp.classList.toggle('active-pop-up');
-    closeIcon.classList.toggle('close');
-}); closeIcon.addEventListener('click', () => {
-    popUp.classList.toggle('active-pop-up'); closeIcon.classList.toggle('close');
+  popUp.classList.toggle('active-pop-up');
+  closeIcon.classList.toggle('close');
 });
-
-menuItems.forEach(item => {
-    item.addEventListener('click', () => {
-        menuList.classList.remove('show');
-    });
+closeIcon.addEventListener('click', () => {
+  popUp.classList.toggle('active-pop-up');
+  closeIcon.classList.toggle('close');
+});
+navLinks.forEach((nav) => {
+  nav.addEventListener('click', () => {
+    popUp.classList.remove('active-pop-up');
+    closeIcon.classList.remove('close');
+  });
 });
